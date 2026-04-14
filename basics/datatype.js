0b96246a2c0722b4b13826b0427884b0d19c5729
BISWABHUSHAN SWAIN <157818832+biswa220124@users.noreply.github.com>
2026-04-14 04:12:08 +0000

    learnedTypeConversion

---
+++ basics/datatype.js
@@ -0,0 +1,5 @@
+const age = 29;
+var city= "Bhubaneswar";
+let country = "india";
+console.table([age,city,country]);
+// dont use var as it's scope is not defined;
---
+++ basics/typeConversion.js
@@ -0,0 +1,11 @@
+let age = 20;
+// console.log(typeof age);
+let ageConv = String(age);
+// console.log(typeof ageConv);
+let cond = true;
+console.log(typeof cond);
+let condConv = Number(cond);
+console.log(typeof(condConv));
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
