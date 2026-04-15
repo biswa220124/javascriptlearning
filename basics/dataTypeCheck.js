02755044365e1dcbf4db18c9f08753704904aa0f
BISWABHUSHAN SWAIN <157818832+biswa220124@users.noreply.github.com>
2026-04-15 12:05:34 +0000

    learnt strings and methods

---
+++ basics/dataTypeCheck.js
@@ -0,0 +1,4 @@
+let userEmail;
+console.log(typeof userEmail);
+let arr = ["Hi","Bye"];
+console.log(typeof arr);
---
+++ basics/strings.js
@@ -0,0 +1,7 @@
+const repo = 69;
+const name = "Biswa";
+// console.log(`My name is ${name} and i have ${repo} repositries on Github`);
+const city = new String();
+const bigWord = new String(`The Big Brown Fox Has Twenty Kids    `);
+
+console.table([bigWord.charAt(5), bigWord.replaceAll('B','D'), bigWord.trimEnd(), bigWord.slice(2,4)]);
