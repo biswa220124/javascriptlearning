857e16b1d5aea1de281cc2092c921ecd5ece0371
BISWABHUSHAN SWAIN <157818832+biswa220124@users.noreply.github.com>
2026-04-16 07:32:52 +0000

    Learnt Arrays

---
+++ basics/numbersAndMaths.js
@@ -0,0 +1,4 @@
+const num = new Number(69000);
+// console.log(num);
+console.log(Math.floor((Math.random())*10+1));
+console.log(num.toLocaleString('en-IN'))
---
+++ basics_01/arrays.js
@@ -0,0 +1,22 @@
+let myArray = [1,2,3,4,5,6];
+console.log("orginal Array",myArray);
+let slicedArray= myArray.slice(0,2);
+console.log("Sliced Array",slicedArray);
+console.log("Spliced Array",myArray.splice(1,3));
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
