259289049eb1bb04ba8cc2cd687375785c9b602e
BISWABHUSHAN SWAIN <157818832+biswa220124@users.noreply.github.com>
2026-05-03 13:13:20 +0000

    control flow statement

---
+++ controlFlow/01.js
@@ -0,0 +1,18 @@
+if(true){
+    console.log('Hello World');
+}
+age = 15;
+if(age>16){
+    console.log('You are allowed');
+    
+}else{
+    console.log('You are not allowed');
+    
+}
+isUserAge = true; isUserDebitCard = true;
+if(isUserAge && isUserDebitCard){
+    console.log('You are allowed to purchase');
+    
+}
+
+if(2==='2') console.log("Hi");
